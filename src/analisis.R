6836b79c8020b9ccdb1dfa8c605cba8a550eed14
Sophia Gamarro <lsophiagr@gmail.com>
2020-09-04 21:57:54 -0600

    update análisis

--- src/analisis.R
+++ src/analisis.R
@@ -1,1 +1,13 @@
-a <- "Q onda como vas"
+library(RMySQL)
+library("DBI")
+library(flexdashboard)
+library(readr)
+library(dplyr)
+library(DT)
+mydb = dbConnect(MySQL(), user = 'root', password = 'root123', dbname='academatica_db', host = '172.19.0.2')
+
+stats<- as.data.frame(mydb%>% tbl("stats"))
+videos<- as.data.frame(mydb%>% tbl("videos"))
+metadata<- as.data.frame(mydb%>% tbl("metadata"))
+
+                         
